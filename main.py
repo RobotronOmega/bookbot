21222db8df3a1a77341c6b120f6dfc1ee84b69cb
RobotronOmega <kevincperry@hotmail.com>
2025-07-20 16:53:23 -0400

    basic version of bookbot done

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from stats import get_num_words
 from stats import get_book_text
+from stats import count_characters
+from stats import sorted_dict
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
- #   split_text = raw_text.split()
-    print(f"{get_num_words(book_text)} words found in the document")
+    book_path = "books/frankenstein.txt"
+#   Get text from book    
+    book_text = get_book_text(book_path)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(book_text)} total words")
+    print("--------- Character Count -------")
+    count = count_characters(book_text)
+    sdict = sorted_dict(count)
+    for i in range(len(sdict)):
+        if sdict[i]["char"].isalpha():
+            print(f"{sdict[i]["char"]}: {sdict[i]["num"]}")
+    print("============= END ===============")
     return None
 
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,27 @@ def get_book_text(fp):
 
 def get_num_words(text):
     return len(text.split())
+
+def count_characters(text):
+    char_count = {}
+    for char in text:
+        if char.lower() not in char_count:
+            char_count[char.lower()] = 1
+        else:
+            char_count[char.lower()] += 1
+    return char_count
+
+def sort_on(list):
+    return list["num"]
+
+
+def sorted_dict(count):
+    slist = []
+    for key, value in count.items():
+        temp_dict = {
+            "char": key,
+            "num": value
+        }
+        slist.append(temp_dict)
+    slist.sort(reverse=True, key=sort_on)
+    return slist
